2978c51188264d1815e4d324978dd0b1132be4e7
dimasm93 <software.dimas_m@icloud.com>
2019-09-17 11:33:31 +0700

    enable server context path

--- src/main/java/com/maryanto/dimas/example/config/WebFluxConfiguration.java
+++ src/main/java/com/maryanto/dimas/example/config/WebFluxConfiguration.java
@@ -1,10 +1,28 @@
 package com.maryanto.dimas.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 @EnableWebFlux
 public class WebFluxConfiguration implements WebFluxConfigurer {
+
+    @Bean
+    public WebFilter contextPathWebFilter(@Value("${server.servlet.context-path}") String contextPath) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (request.getURI().getPath().startsWith(contextPath)) {
+                return chain.filter(
+                        exchange.mutate()
+                                .request(request.mutate().contextPath(contextPath).build())
+                                .build());
+            }
+            return chain.filter(exchange);
+        };
+    }
 }
