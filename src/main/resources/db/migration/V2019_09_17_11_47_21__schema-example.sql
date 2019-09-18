dbc6e2aa0a18972104e51d8303f9d726c01dac97
dimasm93 <software.dimas_m@icloud.com>
2019-09-18 16:25:40 +0700

    update query

--- src/main/resources/db/migration/V2019_09_17_11_47_21__schema-example.sql
+++ src/main/resources/db/migration/V2019_09_17_11_47_21__schema-example.sql
@@ -11,9 +11,9 @@ create table examples
 );
 
 insert into examples(id, name, age, birthdate, balance, created_date)
-values (uuid_generate_v4(), 'Dimas Maryanto', 20, now(), 0, now()),
-       (uuid_generate_v4(), 'Hari Sapto Adi', 20, now(), 0, now()),
-       (uuid_generate_v4(), 'Muhamad Yusuf', 20, now(), 0, now()),
-       (uuid_generate_v4(), 'Prima Jakaria', 20, now(), 0, now()),
-       (uuid_generate_v4(), 'Andri', 20, now(), 0, now()),
-       (uuid_generate_v4(), 'Gufron', 20, now(), 0, now());
+values (uuid_generate_v4(), 'Dimas Maryanto', 21, now(), 0, now()),
+       (uuid_generate_v4(), 'Hari Sapto Adi', 22, now(), 0, now()),
+       (uuid_generate_v4(), 'Muhamad Yusuf', 23, now(), 0, now()),
+       (uuid_generate_v4(), 'Prima Jakaria', 24, now(), 0, now()),
+       (uuid_generate_v4(), 'Andri', 25, now(), 0, now()),
+       (uuid_generate_v4(), 'Gufron', 26, now(), 0, now());
