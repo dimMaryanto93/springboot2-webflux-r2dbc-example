61ee2ae63829621bb047ff6f9f6fe2e244571f99
dimasm93 <software.dimas_m@icloud.com>
2019-09-18 15:09:17 +0700

    enable reactive crud repository

--- src/main/java/com/maryanto/dimas/example/WebfluxExampleApplication.java
+++ src/main/java/com/maryanto/dimas/example/WebfluxExampleApplication.java
@@ -2,6 +2,7 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
--- src/main/java/com/maryanto/dimas/example/config/DatasourceConfiguration.java
+++ src/main/java/com/maryanto/dimas/example/config/DatasourceConfiguration.java
@@ -3,27 +3,39 @@ package com.maryanto.dimas.example.config;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.core.DatabaseClient;
-
-import java.sql.SQLException;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
+@EnableR2dbcRepositories
 @FlywayDataSource
-public class DatasourceConfiguration {
+public class DatasourceConfiguration extends AbstractR2dbcConfiguration {
+
+    @Value("${database.host}")
+    String host;
+    @Value("${database.port}")
+    Integer port;
+    @Value("${database.name}")
+    String databaseName;
+    @Value("${database.auth.username}")
+    String username;
+    @Value("${database.auth.password}")
+    String password;
 
     @Bean
-    @Primary
-    public ConnectionFactory connectionFactory(
-            @Value("${database.host}") String host,
-            @Value("${database.port}") Integer port,
-            @Value("${database.name}") String databaseName,
-            @Value("${database.auth.username}") String username,
-            @Value("${database.auth.password}") String password) throws SQLException {
+    public DatabaseClient databaseClient(@Qualifier("connectionFactory") ConnectionFactory cf) {
+        return DatabaseClient.create(cf);
+    }
+
+    @Override
+    @Bean
+    public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration.builder()
                         .host(host)
@@ -34,10 +46,4 @@ public class DatasourceConfiguration {
                         .build()
         );
     }
-
-    @Bean
-    public DatabaseClient databaseClient(ConnectionFactory cf) {
-        return DatabaseClient.create(cf);
-    }
-
 }
